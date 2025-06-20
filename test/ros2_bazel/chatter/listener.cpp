17eb034fe3c7833291993644935fb52b2a4336ef
Andrea Nisticò <andrea.nistic@gmail.com>
2025-06-20 13:14:14 +0000

    intgrates llvm formatting tools

--- test/ros2_bazel/chatter/listener.cpp
+++ test/ros2_bazel/chatter/listener.cpp
@@ -18,22 +18,26 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-class MinimalSubscriber : public rclcpp::Node {
- public:
-  MinimalSubscriber() : Node("minimal_subscriber") {
-    subscription_ = create_subscription<std_msgs::msg::String>(
-        "topic", 10, [this](std_msgs::msg::String::UniquePtr msg) {
-          RCLCPP_INFO(get_logger(), "I heard: '%s'", msg->data.c_str());
-        });
-  }
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber() : Node("minimal_subscriber")
+    {
+        subscription_ = create_subscription<std_msgs::msg::String>(
+            "topic",
+            10,
+            [this](std_msgs::msg::String::UniquePtr msg)
+            { RCLCPP_INFO(get_logger(), "I heard: '%s'", msg->data.c_str()); });
+    }
 
- private:
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char* argv[]) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
--- test/ros2_bazel/chatter/talker.cpp
+++ test/ros2_bazel/chatter/talker.cpp
@@ -22,32 +22,36 @@
  * function to shorten the callback syntax, at the expense of making the
  * code somewhat more difficult to understand at first glance. */
 
-class MinimalPublisher : public rclcpp::Node {
-public:
-  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-    declare_parameter("callback_period_ms", 500);
-    auto callback_period_ms = get_parameter("callback_period_ms").as_int();
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher() : Node("minimal_publisher"), count_(0)
+    {
+        declare_parameter("callback_period_ms", 500);
+        auto callback_period_ms = get_parameter("callback_period_ms").as_int();
 
-    publisher_ = create_publisher<std_msgs::msg::String>("topic", 10);
-    auto timer_callback = [this]() -> void {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    };
-    timer_ = create_wall_timer(std::chrono::milliseconds(callback_period_ms),
-                               timer_callback);
-  }
+        publisher_ = create_publisher<std_msgs::msg::String>("topic", 10);
+        auto timer_callback = [this]() -> void
+        {
+            auto message = std_msgs::msg::String();
+            message.data = "Hello, world! " + std::to_string(count_++);
+            RCLCPP_INFO(get_logger(), "Publishing: '%s'", message.data.c_str());
+            publisher_->publish(message);
+        };
+        timer_ = create_wall_timer(
+            std::chrono::milliseconds(callback_period_ms), timer_callback);
+    }
 
-private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
 };
 
-int main(int argc, char *argv[]) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
