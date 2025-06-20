c1e0746f6dc7cd1e277d067bd5a1e0d08b047dc1
Marco Kanneworff <marco.kanneworff@stellantis.com>
2025-06-20 12:05:42 +0200

    Add simulation parameter parser

---
+++ simulation/simulation_parameters.py
@@ -0,0 +1,25 @@
+import pybullet
+import yaml
+from dataclasses import dataclass, fields
+
+@dataclass
+class RuntimeParams:
+    real_time: bool
+    time_step: float
+    max_physics_solver_iterations: int
+
+class SimulationParameters:
+    def __init__(self, parameter_file_path):
+        self.physics_engine_connection = pybullet.SHARED_MEMORY
+        self.runtime_params: RuntimeParams
+        self._parse_parameter_file(parameter_file_path)
+        
+    def _parse_parameter_file(self, parameter_file_path):
+        with open(parameter_file_path, 'r') as file:
+            params = yaml.safe_load(file)
+            for field in fields(RuntimeParams):
+                if field.name not in params:
+                    raise KeyError(f"Parameter file {parameter_file_path}: Missing required parameter: {field.name}")
+            # Filter out extra keys
+            filtered_params = {field.name: params[field.name] for field in fields(RuntimeParams)}
+            self.runtime_params = RuntimeParams(**filtered_params)
