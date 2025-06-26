70fa3e9cc98ed58ecdfc81099a33061fb878d1a7
Marco Kanneworff <marco.kanneworff@stellantis.com>
2025-06-26 11:57:01 +0200

    Add spotMicro sim model

---
+++ simulation/spotmicro_sim_model.py
@@ -0,0 +1,122 @@
+from dataclasses import dataclass
+import pybullet
+import os
+
+URDF_PATH = os.path.dirname(__file__) + "/robot_model/spot.xml"
+
+VAR_INDEX_TO_JOINT_NAME = {
+    0: "front_left_shoulder",
+    1: "front_left_leg",
+    2: "front_left_foot",
+    3: "front_right_shoulder",
+    4: "front_right_leg",
+    5: "front_right_foot",
+    6: "rear_left_shoulder",
+    7: "rear_left_leg",
+    8: "rear_left_foot",
+    9: "rear_right_shoulder",
+    10: "rear_right_leg",
+    11: "rear_right_foot",
+}
+
+
+@dataclass
+class BaseState:
+    position: tuple[float, float, float]
+    orientation: tuple[float, float, float, float]
+    linear_velocity: tuple[float, float, float]
+    angular_velocity: tuple[float, float, float]
+
+
+class SpotMicroSimModel:
+    def __init__(self, name, urdf_path):
+        self.__name = name
+        self.__agent = pybullet.loadURDF(
+            urdf_path,
+            useFixedBase=False,
+            useMaximalCoordinates=False,
+            flags=pybullet.URDF_USE_SELF_COLLISION,
+        )
+        self.__control_input = []
+        self.__control_mode = pybullet.POSITION_CONTROL
+        pybullet.changeDynamics(self.__agent, -1, lateralFriction=0.8)
+
+        self.joint_mapping = self._get_joint_mapping()
+        for _, joint_name in VAR_INDEX_TO_JOINT_NAME.items():
+            if joint_name not in self.joint_mapping:
+                raise ValueError(
+                    f"Joint name '{joint_name}' not found in the agent's joint mapping."
+                )
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def agent(self) -> int:
+        return self.__agent
+
+    @property
+    def control_mapping(self) -> dict[int, str]:
+        return VAR_INDEX_TO_JOINT_NAME
+
+    def _get_joint_mapping(self) -> dict[str, int]:
+        nJoints = pybullet.getNumJoints(self.__agent)
+        joint_mapping = {}
+
+        for i in range(nJoints):
+            jointInfo = pybullet.getJointInfo(self.__agent, i)
+            joint_mapping[jointInfo[1].decode("UTF-8")] = jointInfo[0]
+        return joint_mapping
+
+    def set_control(
+        self, control_input: list[float], control_mode=pybullet.POSITION_CONTROL
+    ) -> None:
+        if len(control_input) != len(VAR_INDEX_TO_JOINT_NAME):
+            raise ValueError(
+                f"Control input must have {len(VAR_INDEX_TO_JOINT_NAME)} elements."
+            )
+        self.__control_input = control_input
+        self.__control_mode = control_mode
+
+    def apply_control(self) -> None:
+        if not self.__control_input:
+            raise ValueError(
+                "Control input is not set. Please set control input first."
+            )
+
+        for var_index in range(len(self.__control_input)):
+            joint_name = VAR_INDEX_TO_JOINT_NAME[var_index]
+            joint_index = self.joint_mapping[joint_name]
+            pybullet.setJointMotorControl2(
+                bodyIndex=self.__agent,
+                jointIndex=joint_index,
+                controlMode=self.__control_mode,
+                targetPosition=self.__control_input[var_index],
+            )
+
+    def get_joint_states(self) -> list[tuple]:
+        joint_states = []
+        for var_index in range(len(VAR_INDEX_TO_JOINT_NAME)):
+            joint_name = VAR_INDEX_TO_JOINT_NAME[var_index]
+            joint_index = self.joint_mapping[joint_name]
+            joint_state = pybullet.getJointState(self.__agent, joint_index)
+            joint_states.append(joint_state)
+        return joint_states
+
+    def get_base_state(self) -> BaseState:
+        base_position, base_orientation = pybullet.getBasePositionAndOrientation(
+            self.__agent
+        )
+        base_linear_velocity, base_angular_velocity = pybullet.getBaseVelocity(
+            self.__agent
+        )
+        return BaseState(
+            position=base_position,
+            orientation=base_orientation,
+            linear_velocity=base_linear_velocity,
+            angular_velocity=base_angular_velocity,
+        )
+
+    def step(self):
+        self.apply_control()
