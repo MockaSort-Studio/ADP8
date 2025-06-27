f7edeeb3083d91f70b83f330427fcec69db94054
Marco Kanneworff <marco.kanneworff@stellantis.com>
2025-06-27 18:11:25 +0200

    Added components into main (todo: fix)

--- simulation/simulation_main.py
+++ simulation/simulation_main.py
@@ -1,12 +1,22 @@
 from simulation_parameters import SimulationParameters
+from simulation_runner import SimulationRunner
+from spotmicro_sim_model import SpotMicroSimModel
 import os
-import pybullet
-
 
 DEFAULT_PARAMETER_FILE_PATH = os.path.join(
     os.path.dirname(__file__), "simulation_parameters.yaml"
 )
 
 if __name__ == "__main__":
-    sim_parameters = SimulationParameters(DEFAULT_PARAMETER_FILE_PATH)
-    print(f"Simulation Runtime Parameters: {sim_parameters.runtime_params}")
+    try:
+        sim_parameters = SimulationParameters(DEFAULT_PARAMETER_FILE_PATH)
+        print(f"Simulation Runtime Parameters: {sim_parameters.runtime_params}")
+        sim_runner = SimulationRunner(sim_parameters)
+        spotmicro = SpotMicroSimModel("Spot")
+        sim_runner.start()
+        sim_runner.set_simulated_entity(spotmicro)
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sim_runner.shutdown()
