e463fb0f4736272000bbd145f0cb11b388c565ce
Francesco Petracci <petracci.francesco@gmail.com>
2025-12-09 09:19:35 +0000

    car_state now subscribe to car_control topic and act the commands

--- simulation/car_state.cpp
+++ simulation/car_state.cpp
@@ -1,22 +1,15 @@
+#include "car_msgs/msg/car_state.hpp"
+
 #include <chrono>
 #include <cmath>
 #include <memory>
 
+#include "applications/common_utils.hpp"
+#include "car_msgs/msg/car_control.hpp"
 #include "rclcpp/rclcpp.hpp"
-// #include "std_msgs/msg/float32.hpp"
-#include "car_msgs/msg/car_state.hpp"
 
 #define T_CARSTATE_PUB 10  // ms
 #define T_CARPOSE_PUB 200  // ms
-#define L_WHEELBASE 2.5    // meters
-
-struct CarState
-{
-    double x = 0.0;
-    double y = 0.0;
-    double yaw = 0.0;
-    double d = 0.0;  // steering angle (input)
-};
 
 class CarStatePublisher : public rclcpp::Node
 {
@@ -25,17 +18,25 @@ class CarStatePublisher : public rclcpp::Node
     {
         // declare parameters
         this->declare_parameter("callback_period_ms", T_CARSTATE_PUB);
-        this->declare_parameter("v", 5.0);               // longitudinal speed (m/s)
-        this->declare_parameter("steering_angle", 0.2);  // radians
-
-        // Get parameters
-        v_ = this->get_parameter("v").as_double();
-        state_.d = this->get_parameter("steering_angle").as_double();
         callback_period_ms_ = this->get_parameter("callback_period_ms").as_int();
 
+        // this->declare_parameter("v", 5.0);               // longitudinal speed (m/s)
+        // this->declare_parameter("steering_angle", 0.2);  // radianss
+        // v_ = this->get_parameter("v").as_double();
+        // state_.d = this->get_parameter("steering_angle").as_double();
+
+        // init commands
+        v_cmd_ = 0.2;
+        d_cmd_ = 0.0;
+
         // Publisher
-        publisher_ =
+        car_state_publisher_ =
             this->create_publisher<car_msgs::msg::CarState>("car_state", T_CARSTATE_PUB);
+
+        control_sub_ = this->create_subscription<car_msgs::msg::CarControl>(
+            "car_control",
+            T_CARSTATE_PUB,
+            std::bind(&CarStatePublisher::control_callback, this, std::placeholders::_1));
 
         // Timer callback (simulation loop)
         timer_ = this->create_wall_timer(
@@ -48,10 +49,12 @@ class CarStatePublisher : public rclcpp::Node
     {
         double dt = callback_period_ms_ / 1000.0;  // convert ms → seconds
 
+        state_.d = d_cmd_;
+
         // --- Kinematic bicycle model ---
-        state_.x += v_ * std::cos(state_.yaw) * dt;
-        state_.y += v_ * std::sin(state_.yaw) * dt;
-        state_.yaw += v_ * std::tan(state_.d) / L_WHEELBASE * dt;
+        state_.x += v_cmd_ * std::cos(state_.yaw) * dt;
+        state_.y += v_cmd_ * std::sin(state_.yaw) * dt;
+        state_.yaw += v_cmd_ * std::tan(state_.d) / L_WHEELBASE * dt;
 
         // --- Publish ---
         car_msgs::msg::CarState msg;
@@ -60,13 +63,25 @@ class CarStatePublisher : public rclcpp::Node
         msg.yaw = state_.yaw;
         msg.d = state_.d;
 
-        publisher_->publish(msg);
+        car_state_publisher_->publish(msg);
     }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<car_msgs::msg::CarState>::SharedPtr publisher_;
 
-    CarState state_;
-    double v_;
+    void control_callback(const car_msgs::msg::CarControl::SharedPtr msg)
+    {
+        v_cmd_ = msg->v;
+        d_cmd_ = msg->d;
+    }
+
+    // ROS2 interfaces
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<car_msgs::msg::CarState>::SharedPtr car_state_publisher_;
+    rclcpp::Subscription<car_msgs::msg::CarControl>::SharedPtr control_sub_;
+
+    CarState state_;  // internal state
+
+    double v_cmd_;  // vel command
+    double d_cmd_;  // steering wheel command
+
     int callback_period_ms_;
 };
 
