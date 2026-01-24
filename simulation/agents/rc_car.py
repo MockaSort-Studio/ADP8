dc1de25acfa3cd6da6afeedacfe08e4156022f4e
Francesco Petracci <petracci.francesco@gmail.com>
2026-01-24 12:44:40 +0000

    observation for RCCar

--- simulation/agents/rc_car.py
+++ simulation/agents/rc_car.py
@@ -17,7 +17,26 @@ class RCCar(BaseEntity):
             )
         )
 
-        # TODO: add imu
+        # IMU
+        self.imu = scene.add_sensor(
+            gs.sensors.IMU(
+                entity_idx=self.rc_car_entity.idx,
+                link_idx_local=self.rc_car_entity.get_link("base_link").idx_local,
+                pos_offset=(0.05, 0.0, 0.0),
+                # noise parameters
+                acc_cross_axis_coupling=(0.0, 0.01, 0.02),
+                gyro_cross_axis_coupling=(0.03, 0.04, 0.05),
+                acc_noise=(0.01, 0.01, 0.01),
+                gyro_noise=(0.01, 0.01, 0.01),
+                acc_random_walk=(0.001, 0.001, 0.001),
+                gyro_random_walk=(0.001, 0.001, 0.001),
+                delay=0.01,
+                jitter=0.01,
+                interpolate=True,
+                # visualize
+                draw_debug=True,
+            )
+        )
 
         # now that entity is there, we can access its joints
         self.rear_wheel_idx = [
@@ -29,10 +48,32 @@ class RCCar(BaseEntity):
         ).dofs_idx_local[0]
 
     def get_observation(self):
-        # imu placeholder
+
+        # IMU
+        # true_data = imu.read_ground_truth() # for GT
+        imu_data = self.imu.read()
+        acc = imu_data.lin_acc  # tensor([ax, ay, az])
+        gyro = imu_data.ang_vel  # tensor([wx, wy, wz])
+
+        # steering angle
+        d = self.rc_car_entity.get_dofs_position(self.steering_idx).item()
+        d = d + np.clip(np.random.normal(0.0, 0.01), -0.03, 0.03)  # add noise
+
         # for reference:
-        #   accx, accy, accz, yaw, pitch, roll
-        return [0, 0, 0, 0, 0, 0]
+        #   [
+        #       accx, accy, accz,
+        #       wx, wy, wz,
+        #       d
+        #   ]
+        return [
+            acc[0].item(),
+            acc[1].item(),
+            acc[2].item(),
+            gyro[0].item(),
+            gyro[1].item(),
+            gyro[2].item(),
+            d,
+        ]
 
     def step(self, inputs):
         # for reference: inputs = [steer_torque, rear_axle_torque]
