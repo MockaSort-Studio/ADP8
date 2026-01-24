5177a77abcb5885c8618aa71c991f6c28f65f06d
Francesco Petracci <petracci.francesco@gmail.com>
2026-01-24 13:22:56 +0000

    simulation node with imu

--- simulation/simulation_node.py
+++ simulation/simulation_node.py
@@ -2,29 +2,50 @@ import rclpy
 from rclpy.node import Node
 
 from car_msgs.msg import IMURaw
+from car_msgs.msg import SteeringAngleMes
+
+from simulation.src.simulation_runner import SimulationRunner
+from simulation.agents.rc_car import RCCar
 
 
 class SimulationNode(Node):
 
     def __init__(self):
         super().__init__("simulation_node")
-        self.publisher_ = self.create_publisher(IMURaw, "imu_raw", 10)
-        timer_period = 0.1  # seconds
+        self.IMU_publisher_ = self.create_publisher(IMURaw, "imu_raw", 10)
+        self.d_publisher_ = self.create_publisher(
+            SteeringAngleMes, "steering_angle_measured", 10
+        )
+
+        timer_period = 0.01  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        rccar = RCCar()
+        self.sim = SimulationRunner(agent=rccar, show_viewer=True, dt=timer_period)
 
     def timer_callback(self):
-        msg = IMURaw()
-        msg.acc_x = 0.0
-        msg.acc_y = 10.0
-        msg.acc_z = 20.0
-        msg.gyro_x = 0.0
-        msg.gyro_y = 0.0
-        msg.gyro_z = 0.0
 
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%i"' % self.i)
-        self.i += 1
+        # observations (sim outputs, imu and steering angle)
+        obs = self.sim.get_observations()
+
+        IMUmsg = IMURaw()
+        IMUmsg.acc_x = obs[0]
+        IMUmsg.acc_y = obs[1]
+        IMUmsg.acc_z = obs[2]
+        IMUmsg.gyro_x = obs[3]
+        IMUmsg.gyro_y = obs[4]
+        IMUmsg.gyro_z = obs[5]
+        self.IMU_publisher_.publish(IMUmsg)
+
+        d_msg = SteeringAngleMes()
+        d_msg.d = obs[6]
+        self.d_publisher_.publish(d_msg)
+
+        # get inputs
+
+        inputs = [0, 0]
+
+        # step with inputs [steer_torque, rear_axle_torque]
+        self.sim.step(inputs=inputs)
 
 
 def main(args=None):
